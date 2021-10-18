71f5b039a543a5d53013c904f306a78b156cfe04
Daniel Pires <dfpires@gmail.com>
2021-10-18 11:04:34 -0300

    18-10

---
+++ gitcollection/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// App é um componente funcional
+const App: React.FC = () =>  {
+  return (
+   <h2> Minha primeira aplicação em ReactJS </h2>
+  );
+}
+
+export default App;
