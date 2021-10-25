21f880301954e3ac7bd5a6a7055ddfc35c9a8135
Daniel Pires <dfpires@gmail.com>
2021-10-25 11:11:11 -0300

    25-10

--- gitcollection/src/pages/Dashboard/index.tsx
+++ gitcollection/src/pages/Dashboard/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
-
+import { Title, Form } from './styles'
+import logo from '../../assets/logo.svg'
 export const Dashboard: React.FC = () => {
     return (
-        <h2> Página de Dashboard </h2>
+        <>
+            <img src={logo} alt="GitCollection"/>
+            <Title> Catálogo de repositórios do Github </Title>
+            <Form>
+                <input placeholder="username/repository_name"/>
+                <button type="submit"> Buscar </button>
+            </Form>
+        </>
     )
 }
