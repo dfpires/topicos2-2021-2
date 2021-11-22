96726a0c6bee908d1f4913297229fd3858c61d71
dfpires@gmail.com <dfpires@gmail.com>
2021-11-22 10:43:29 -0300

    22-11

--- gitcollection/src/pages/Dashboard/index.tsx
+++ gitcollection/src/pages/Dashboard/index.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import { Title, Form, Repos } from './styles'
 import logo from '../../assets/logo.svg'
+import {api} from '../../services/api'
+
 export const Dashboard: React.FC = () => {
+
+    // cria tipo de dados para receber as informações do github
+    interface GithubRepository {
+        full_name: string;
+        description: string;
+        owner: {
+            login: string;
+            avatar_url: string;
+        }
+    }
     // cria uma nova variável e atribui o valor vazio pra ela
     const [novoRepositorio, setNovoRepositorio] = React.useState('')
+
+    // cria uma nova variável que guarda todos os repositórios
+    // essa variável é do tipo vetor de GithubRepository e inicia com vazio []
+    const [repositorios, setRepositorios] = React.useState<GithubRepository[]>([])
 
     // função para atribuir valor ao novoRepositorio
     // função é chamada quando usuário digitar na caixa de texto
@@ -12,7 +28,21 @@ export const Dashboard: React.FC = () => {
     }
     // função é chamada quando o botão for pressionado
     function handleAddRepository(event: React.FormEvent<HTMLFormElement>){
-        alert(novoRepositorio)
+        // vai na api do github e consulta pelo repositório de interesse
+        // adiciona ao vetor este repositório
+        event.preventDefault() // não recarrega a página
+        try {
+            api
+            .get<GithubRepository>(`/${novoRepositorio}`)
+            .then (response => {
+                let novoRepo = response.data
+                setRepositorios([...repositorios, novoRepo])
+                console.log(repositorios)
+            })
+        }
+        catch {
+            alert(`Problema ao consultar o repositório`)
+        }
     }
     return (
         <>
@@ -23,13 +53,20 @@ export const Dashboard: React.FC = () => {
                 <button type="submit"> Buscar </button>
             </Form>
             <Repos>
-                <a href="/repo">
-                    <img src="" alt="Repositório"/>
-                    <div>
-                        <strong> username/repositório </strong>
-                        <p> Descrição do repositório</p> 
-                    </div>
-                </a>
+                { /* para cada repositório do vetor */
+                    repositorios.map((repositorio, index) => (
+                        <a href="/repo">
+                        <img 
+                            src={repositorio.owner.avatar_url} 
+                            alt={repositorio.owner.login}/>
+                        <div>
+                            <strong> {repositorio.full_name} </strong>
+                            <p> {repositorio.description}</p> 
+                        </div>
+                    </a>
+                    ) )
+                }
+              
             </Repos>
         </>
     )
